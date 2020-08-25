8d46af3366038f3ed560c543bfb336b76f5a0fda
charleshslin <lin_charles_personal@yahoo.com>
2020-08-25 13:16:52 -0400

    Updated script to get script URL links

--- 3pjsscanner.py
+++ 3pjsscanner.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3.8
 import urllib.request
+import bs4
 
 URL = input("Provide URL to scan: ")
 html = urllib.request.urlopen("http://" + URL).read()
-print(html)
+soup = bs4.BeautifulSoup(html, features='html.parser')
+scripts = soup.find_all('script')
+srcs = [link['src'] for link in scripts if 'src' in link.attrs]
+print(srcs)
